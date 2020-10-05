4972eb680b6b7eeee15da5415ead3b6e5c488951
Vadim, Tsimbal <devoliisher@gmail.com>
2020-10-05 16:22:27 +0300

    Fixed: modal state/props passing and opening.

--- i-need-a-hero/src/buttons/addButton/addButton.jsx
+++ i-need-a-hero/src/buttons/addButton/addButton.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { green, purple } from '@material-ui/core/colors';
-import Dialog from '@material-ui/core/Dialog';
+import React from 'react'
+import {
+  createMuiTheme,
+  withStyles,
+  makeStyles,
+  ThemeProvider
+} from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import { green, purple } from '@material-ui/core/colors'
+import Dialog from '@material-ui/core/Dialog'
 // import Dialog from './../modalCreateHero/modalCreateHero.jsx';
-
 
 const BootstrapButton = withStyles({
   root: {
@@ -26,78 +30,63 @@ const BootstrapButton = withStyles({
       'sans-serif',
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
+      '"Segoe UI Symbol"'
     ].join(','),
     '&:hover': {
       backgroundColor: '#0069d9',
       borderColor: '#0062cc',
-      boxShadow: 'none',
+      boxShadow: 'none'
     },
     '&:active': {
       boxShadow: 'none',
       backgroundColor: '#0062cc',
-      borderColor: '#005cbf',
+      borderColor: '#005cbf'
     },
     '&:focus': {
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-    },
-  },
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)'
+    }
+  }
+})(Button)
 
-})(Button);
+const AddBatton = ({ onClick: handleClick, ...props }) => {
+  const ColorButton = withStyles(theme => ({
+    root: {
+      color: theme.palette.getContrastText(purple[500]),
+      backgroundColor: purple[500],
+      '&:hover': {
+        backgroundColor: purple[700]
+      }
+    }
+  }))(Button)
 
-const AddBatton = ({
-  open,
-  setOpen,
-  handleClickOpen,
-  ...props
-}) => {
+  const useStyles = makeStyles(theme => ({
+    margin: {
+      margin: theme.spacing(1)
+    }
+  }))
 
-const ColorButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText(purple[500]),
-    backgroundColor: purple[500],
-    '&:hover': {
-      backgroundColor: purple[700],
-    },
-  },
+  const theme = createMuiTheme({
+    palette: {
+      primary: green
+    }
+  })
 
-}))(Button);
-
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-}));
-
-
-const theme = createMuiTheme({
-  palette: {
-    primary: green,
-  },
-});
-
-const handleHeroModal = React.useCallback(() => {
-  setOpen(open)
-}, [open, setOpen])
-
-return(
-  <div>
-    <BootstrapButton
-      {...props}
-      setOpen={handleHeroModal}
-      variant="contained"
-      disableRipple
-
+  return (
+    <div>
+      <BootstrapButton
+        {...props}
+        onClick={handleClick}
+        variant='contained'
+        disableRipple
       >
-      I need a hero!!!
-    </BootstrapButton>
-  </div>
-);
-
- }
+        I need a hero!!!
+      </BootstrapButton>
+    </div>
+  )
+}
 AddBatton.defaultProps = {
-   color: 'primary',
-   // className={classes.margin}
- }
+  color: 'primary'
+  // className={classes.margin}
+}
 
-export default AddBatton;
+export default AddBatton
--- i-need-a-hero/src/workFrame/workFrame.jsx
+++ i-need-a-hero/src/workFrame/workFrame.jsx
@@ -1,64 +1,65 @@
-import React from "react";
-import SideBar from './../sideBar/sideBar.jsx';
-import CardHero from './../cardHero/cardHero.jsx';
-import CardAllMight from './../cardHero/cardHeroAllMight/cardHeroAllMight.jsx';
-import AddButton from './../buttons/addButton/addButton.jsx';
-import './workFrame.css';
+import React from 'react'
+import SideBar from './../sideBar/sideBar.jsx'
+import CardHero from './../cardHero/cardHero.jsx'
+import CardAllMight from './../cardHero/cardHeroAllMight/cardHeroAllMight.jsx'
+import AddButton from './../buttons/addButton/addButton.jsx'
+import './workFrame.css'
 import Dialog from './../modalCreateHero/modalCreateHero.jsx'
-
+import ModalCreateHero from '../modalCreateHero/modalCreateHero'
 
 class WorkFrame extends React.Component {
-// Attention! The name from "InputHeroName" must be added to this "state"(state.allMight) to generate the button.
+  // Attention! The name from "InputHeroName" must be added to this "state"(state.allMight) to generate the button.
   state = {
     allMight: [
       {
         name: 'All Might'
-      },
+      }
     ],
 
     moreHero: [],
 
     hero: null,
+
+    modal: false
   }
 
-  handleHero = (hero) => {
+  handleOpenModal = () => this.setState({ modal: true })
+  handleCloseModal = () => this.setState({ modal: false })
+
+  handleHero = hero => {
     this.setState({ hero })
   }
 
-  addHero = (newHero) => {
+  addHero = newHero => {
     this.setState({ newHero })
   }
 
-  render() {
+  render () {
     return (
-      <div className="work_Frame">
+      <div className='work_Frame'>
+        <div className='sideBarGroup'>
+          <div className='sideBarBox'>
+            <SideBar
+              allMight={this.state.allMight}
+              onHeroSelect={this.handleHero}
+              addHero={this.addHero}
+              moreHero={this.state.moreHero}
+            />
+          </div>
 
-<div className="sideBarGroup">
-  <div className="sideBarBox">
-    <SideBar
-      allMight={this.state.allMight}
-      onHeroSelect={this.handleHero}
-      addHero={this.addHero}
-      moreHero={this.state.moreHero}
-    />
-  </div>
+          {this.state.hero ? <CardAllMight hero={this.state.hero} /> : null}
+        </div>
 
-  {this.state.hero ? (
-    <CardAllMight hero={this.state.hero} />
-  ) : null}
+        <div className='addButtonBox'>
+          <AddButton onClick={this.handleOpenModal} />
+        </div>
 
-</div>
-
-    <div className="addButtonBox">
-
-<AddButton></AddButton>
-
-    </div>
-
-
-
+        <ModalCreateHero
+          open={this.state.modal}
+          onClose={this.handleCloseModal}
+        />
       </div>
-    );
+    )
   }
 }
-export default WorkFrame;
+export default WorkFrame
