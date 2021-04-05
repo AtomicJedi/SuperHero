ea152a76f8bef1f4de9e5885eab7b35ef5c2e24f
AtomicJedi <AtomicJedi@i.ua>
2021-04-05 18:19:50 +0300

    Add Docker Add server, MongoDB v2

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,12 +1,14 @@
 set -e
 #use $MONGO_INITDB_DATABASE
-echo 'FROG SAY QWA QWA QWAQWA BEACH!!!!!'
+
+echo $MONGO_INITDB_ROOT_USERNAME
+
 mongo <<EOF
 use test
 
 db.createUser({
-  user: 'db',
-  pwd: '123456',
+  user: '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
   roles: [{
     role: 'readWrite',
     db: 'db'
